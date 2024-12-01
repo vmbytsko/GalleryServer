e807cb0ab9481f4f39df18dad658305eb01c8f55
Vyacheslav Bytsko <169024760+vmbytsko@users.noreply.github.com>
2024-12-02 01:30:36 +0300

    username and not user_id

--- api/v1/secret.py
+++ api/v1/secret.py
@@ -3,7 +3,7 @@ from classes.user import get_user_from_token_info
 
 def search(token_info) -> str:
     return """
-    You are user_id {user} and the secret is 'wbevuec'.
+    You are username {user} and the secret is 'wbevuec'.
     Decoded token claims: {token_info}.
     """.format(
         user=get_user_from_token_info(token_info).username, token_info=token_info
